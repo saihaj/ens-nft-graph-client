5e011a35ddb6f2bcd20949e7b22e84a268c60b17
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-04-23 01:05:13 -0400

    Fix Type Merging (#1)
    * Fixes
    
    * Re-enable batching'
    
    * Delete yarn.lock
    
    Co-authored-by: Saihajpreet Singh <saihajpreet.singh@gmail.com>

--- resolvers.ts
+++ resolvers.ts
@@ -3,25 +3,14 @@ import { print } from 'graphql'
 
 const resolvers: Resolvers = {
   Query: {
-    findUserEns: async (root, { name }, ctx, info) => {
-      const users = await ctx.ENS.Query.ENS_domains({
-        root,
-        args: { where: { name } },
-        context: ctx,
-        info,
-        selectionSet: (selectionSetFromRoot) => {
-          ctx.logger.info(print(selectionSetFromRoot))
-          return /* GraphQL */ `{ owner ${print(selectionSetFromRoot)} }`
-        },
-        valuesFromResults: (results) => {
-          ctx.logger.info(JSON.stringify(results))
-          return results.map((result) => result.owner)
-        },
-      })
-      console.log(JSON.stringify(users))
-      const user = users[0]?.owner || null
-      return user
-    },
+    findUserEns: async (root, { name }, context, info) => context.ENS.Query.ENS_domains({
+      root,
+      args: { where: { name } },
+      context,
+      info,
+      selectionSet: (selectionSetFromRoot) => /* GraphQL */ `{ owner ${print(selectionSetFromRoot)} }`,
+      valuesFromResults: (results) => results?.[0]?.owner,
+    }),
   },
 }
 
