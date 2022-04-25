9170155116698d2b9cd7043fcfd1e0fe58175f07
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-04-25 09:36:28 -0400

    update deps

--- resolvers.ts
+++ resolvers.ts
@@ -3,14 +3,15 @@ import { print } from 'graphql'
 
 const resolvers: Resolvers = {
   Query: {
-    findUserEns: async (root, { name }, context, info) => context.ENS.Query.ENS_domains({
-      root,
-      args: { where: { name } },
-      context,
-      info,
-      selectionSet: (selectionSetFromRoot) => /* GraphQL */ `{ owner ${print(selectionSetFromRoot)} }`,
-      valuesFromResults: (results) => results?.[0]?.owner,
-    }),
+    findUserEns: async (root, { name }, context, info) =>
+      context.ENS.Query.ENS_domains({
+        root,
+        args: { where: { name } },
+        context,
+        info,
+        selectionSet: (selectionSetFromRoot) => /* GraphQL */ `{ owner ${print(selectionSetFromRoot)} }`,
+        valuesFromResults: (results) => results?.[0]?.owner,
+      }),
   },
 }
 
