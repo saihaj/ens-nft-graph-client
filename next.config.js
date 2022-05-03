e58d714b352a8c686aeb911d9143ad62bd9a4409
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-05-03 09:24:25 -0400

    transpile undici
    Undici is not really used but bundler thinks it is going to be used in node 14 so we just transpile it.
    
    trying

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const withTM = require('next-transpile-modules')(['undici']) // pass the modules you would like to see transpiled
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig)
