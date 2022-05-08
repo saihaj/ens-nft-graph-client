38b8b455023ffa1f2d1ed8ff6ef4b17ff082aa0a
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-05-08 15:37:24 -0400

    create a helper query to get user from either address or ENS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,52 +5,48 @@ import { useQuery } from 'urql'
 import styles from '../styles/Home.module.css'
 
 const query = /* GraphQL */ `
-  query UserWithENS {
-    findUserEns(name: "saihaj.eth") {
+  query User($name: String, $address: String) {
+    findUser(input: { name: $name, address: $address }) {
       ERC721tokens {
         contract {
           id
+          name
         }
       }
       domains {
         id
-      }
-    }
-  }
-`
-const tempQuery = /* GraphQL */ `
-  query UserWithENS($name: String!) {
-    ENS_domains(where: { name: $name }) {
-      owner {
-        id
-        ERC721tokens {
-          contract {
-            id
-            name
-          }
-        }
+        name
       }
     }
   }
 `
 
 export default function Home() {
-  const [ensName, setEnsName] = useState('saihaj.eth')
+  const [input, setInput] = useState<{ name: string | null; address: string | null }>({
+    name: 'saihaj.eth',
+    address: null,
+  })
   const [{ data, fetching }] = useQuery({
-    query: tempQuery,
+    query: query,
     variables: {
-      name: ensName,
+      name: input.name,
+      address: input.address,
     },
   })
 
   return (
     <div>
       <input
-        title="ENS"
+        title="ENS or Public Address"
         placeholder="ENS"
-        value={ensName}
+        value={input.name || input.address}
         onChange={(e) => {
-          setEnsName(e.target.value)
+          const value = e.target.value
+          if (value.includes('.eth')) {
+            setInput({ name: value, address: null })
+          } else {
+            setInput({ name: null, address: value })
+          }
         }}
       />
       {!fetching && <pre>{JSON.stringify(data, null, 2)}</pre>}
